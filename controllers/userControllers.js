e0234a3ef9a62e4532b917a86697826e9b5db0bd
muhammadsuryo31 <muhammadsuryo31@gmail.com>
2021-03-21 10:16:52 +0700

    patch user-controller-server

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -18,17 +18,16 @@ class UserController {
     static async login (req, res, next) {
         try {
             const {email, password} = req.body
-            const user = User.findOne({where:{email}})
+            const user = await User.findOne({where:{email}})
             if(!user) throw {name:'customError', code :401,msg: 'Invalid email or password'}
-            const compare = comparePassword(password, user.password)
+            const compare = await comparePassword(password, user.password)
 			if(!compare) throw {name:'customError', code :401,msg: 'Invalid email or password'}
-            const accsess_token = generateToken({
+            const access_token = generateToken({
 				id:user.id,
 				email:user.email,
-                role:user.role,
                 name:user.name
 			})
-			res.status(200).json({accsess_token})
+			res.status(200).json({access_token})
         } catch (err) {
             next(err)
         }
