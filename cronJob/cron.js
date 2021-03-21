ff7bed2e619b6f58328a61183b1fc51635088bdc
amoswijaya <amoswijaya728@gmail.com>
2021-03-21 15:51:11 +0700

    siap cron job

---
+++ cronJob/cron.js
@@ -0,0 +1,12 @@
+const CronJob = require('cron').CronJob;
+time = '15.23'
+
+let [hour, minute] = time.split(".")
+let timeParsed = `${minute} ${hour} * * * `
+var job = new CronJob(timeParsed, function() {
+  
+
+
+
+}, null, true, 'Asia/Jakarta');
+job.start();
